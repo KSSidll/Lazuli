53d96bbcffda5b7056e83102b901685d421af21c
Filip Kociok <filipkociok@gmail.com>
2023-03-27 12:22:45 +0200

    Adds model folder with user related models

---
+++ LazuliLibrary/Models/CompanyModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazuliLibrary.Models
+{
+    public class CompanyModel
+    {
+        public string Name { get; set; }
+        public string CatchPhrase { get; set; }
+        public string BusinessStrategy { get; set; }
+    }
+}
---
+++ LazuliLibrary/Models/GeoLocationModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazuliLibrary.Models
+{
+    public class GeoLocationModel
+    {
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+    }
+}
