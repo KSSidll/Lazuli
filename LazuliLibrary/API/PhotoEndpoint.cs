aebc501f7d114ec226cd0acd922e3738b56bf153
Filip Kociok <filipkociok@gmail.com>
2023-03-28 14:22:46 +0200

    Adds PhotoEndpoint with methods to get all photos or filtered either by photoId or albumId

---
+++ LazuliLibrary/API/PhotoEndpoint.cs
@@ -0,0 +1,80 @@
+﻿using LazuliLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazuliLibrary.API
+{
+    public class PhotoEndpoint
+    {
+        private readonly ApiHelper _apiHelper;
+        private const string _page = "posts";
+
+        public PhotoEndpoint(ApiHelper apiHelper)
+        {
+            _apiHelper = apiHelper; 
+        }
+
+        public async Task<List<PhotoModel>> GetAll()
+        {
+                // checks if there are null values
+            ApiHelper.ApiHelperValidator(_apiHelper);
+
+            using (HttpResponseMessage response = await _apiHelper!.ApiClient!.GetAsync($"/{_page}"))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsAsync<List<PhotoModel>>();
+
+                    return result;
+                }
+                else
+                {
+                    throw new HttpRequestException(response.ReasonPhrase);
+                }
+            }
+        }
+
+        public async Task<PhotoModel> GetByPhotoId(int photoId)
+        {
+            // checks if there are null values
+            ApiHelper.ApiHelperValidator(_apiHelper);
+
+            using (HttpResponseMessage response = await _apiHelper!.ApiClient!.GetAsync($"/{_page}/{photoId}"))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsAsync<PhotoModel>();
+
+                    return result;
+                }
+                else
+                {
+                    throw new HttpRequestException(response.ReasonPhrase);
+                }
+            }
+        }
+
+        public async Task<List<PhotoModel>> GetByPostId(int postId)
+        {
+            // checks if there are null values
+            ApiHelper.ApiHelperValidator(_apiHelper);
+
+            using (HttpResponseMessage response = await _apiHelper!.ApiClient!.GetAsync($"/{_page}?postId={postId}"))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsAsync<List<PhotoModel>>();
+
+                    return result;
+                }
+                else
+                {
+                    throw new HttpRequestException(response.ReasonPhrase);
+                }
+            }
+        }
+    }
+}
