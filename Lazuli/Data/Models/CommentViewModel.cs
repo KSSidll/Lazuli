1908d3144c2265f5673031586fe6320dc9e7c2e8
Filip Kociok <filipkociok@gmail.com>
2023-04-03 20:04:03 +0200

    Adds a frontend comment and post model

---
+++ Lazuli/Data/Models/CommentViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Lazuli.Data.Models
+{
+	public class CommentViewModel
+	{
+		public string? Email { get; set; }
+		public int CommentId { get; set; }
+		public string? Body { get; set; }
+	}
+}
---
+++ Lazuli/Data/Models/PostViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Lazuli.Data.Models;
+using LazuliLibrary.Models;
+
+namespace Lazuli.Data.Model
+{
+	public class PostViewModel
+	{
+		public int Id { get; set; }
+		public int UserId { get; set; }
+		public string? Username { get; set; }
+		public string? Title { get; set; }
+		public List<CommentViewModel>? Comments { get; set; }
+	}
+}
