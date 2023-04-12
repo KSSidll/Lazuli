ca0f2c0c2de6715a56b7c6e050b3b191576af87b
KSSidll <kolasasidll@gmail.com>
2023-04-12 10:59:58 +0200

    Add Fake Endpoints for Albums, Comments, Photos and Posts.
    Signed-off-by: KSSidll <kssidll@users.noreply.github.com>

---
+++ LazuliTest/Fakes/FakeCommentEndpoint.cs
@@ -0,0 +1,27 @@
+﻿using LazuliLibrary.API.Endpoints;
+using LazuliLibrary.Models;
+using LazuliTest.Helpers;
+
+namespace LazuliTest.Fakes;
+
+public class FakeCommentEndpoint : ICommentEndpoint
+{
+	public async Task<List<CommentModel>> GetAll()
+	{
+		return await Task.Run(TestDataHelper.GetFakeCommentModelList);
+	}
+
+	public async Task<CommentModel?> GetByCommentId(int commentId)
+	{
+		return await Task.Run(() => { return TestDataHelper.GetFakeCommentModelList().First(x => x.Id == commentId); });
+	}
+
+	public async Task<List<CommentModel>> GetByPostId(int postId)
+	{
+		return (List<CommentModel>) await Task.Run(() =>
+		{
+			return TestDataHelper.GetFakeCommentModelList()
+								 .Where(x => x.PostId == postId);
+		});
+	}
+}
---
+++ LazuliTest/Fakes/FakePostEndpoint.cs
@@ -0,0 +1,42 @@
+﻿using LazuliLibrary.API.Endpoints;
+using LazuliLibrary.Models;
+using LazuliTest.Helpers;
+
+namespace LazuliTest.Fakes;
+
+public class FakePostEndpoint : IPostEndpoint
+{
+	public int RecordLimit { get; set; } = 5;
+	public int StartIndex { get; set; } = 1;
+
+	public async Task<List<PostModel>> GetAll()
+	{
+		return await Task.Run(TestDataHelper.GetFakePostModelList);
+	}
+
+	public async Task<PostModel?> GetByPostId(int postId)
+	{
+		return await Task.Run(() => { return TestDataHelper.GetFakePostModelList().First(x => x.Id == postId); });
+	}
+
+	public async Task<List<PostModel>> GetByUserId(int userId)
+	{
+		return (List<PostModel>) await Task.Run(() =>
+		{
+			return TestDataHelper.GetFakePostModelList()
+								 .Where(x => x.UserId == userId);
+		});
+	}
+
+	public async Task<List<PostModel>> GetPartially()
+	{
+		return await Task.Run(() =>
+		{
+			List<PostModel> result = TestDataHelper.GetFakePostModelList().GetRange(StartIndex - 1, RecordLimit);
+
+			StartIndex += RecordLimit;
+
+			return result;
+		});
+	}
+}
